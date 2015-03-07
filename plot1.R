0d5a5641994be90ef99160e80d0630d5538dad7f
Maheswari <reach.maheshwari@gmail.com>
2015-03-07 08:18:32 +0530

    cleanup

--- plot1.R
+++ plot1.R
@@ -1,11 +1,14 @@
 plot1 <- function(){
     ##read input data
-    df<-read.table("exdata-data-household_power_consumption/household_power_consumption.txt", header = T, sep=";", comment.char="%", 
+    df<-read.table("household_power_consumption.txt", header = T, sep=";", comment.char="%", 
                    stringsAsFactors=FALSE, na.strings="?")
     dataplot<- subset(df, df$Date=="1/2/2007"|df$Date=="2/2/2007") 
-    jpeg('plot1.jpg')
-    histinfo <-hist(dataplot$Global_active_power,main="Global Active Power",xlab="Global Active Power",col="red")
-    print(histinfo)
+    #file to store the plot   
+    jpeg('plot1.jpg',width=480,height=480,units='px')
+    par(mfrow=c(1,1))
+    hist(dataplot$Global_active_power,main="Global Active Power (killowatts)",xlab="Global Active Power",col="red")
+    
+    #close the graphical device
     dev.off()
-    dataplot
+    
 }
